45d9f8f7daad651e014d4ec863ba0eb9db16c837
DhruvRSeanergy <dhruv.r@seanergydigital.com>
2020-02-27 14:03:05 +0530

    Mailers configured

--- app/mailers/member_mailer.rb
+++ app/mailers/member_mailer.rb
@@ -1,7 +1,7 @@
 class MemberMailer < ApplicationMailer
-	def success_add(group, profile_name, mail_ids)
-		@profile_name=profile_name
-		@group=group
-		mail(to: mail_ids, subject: "Access to the #{group.name} group was granted")
-	end
+  def success_add(group, profile_name, mail_ids)
+    @profile_name=profile_name
+    @group=group
+    mail(to: mail_ids, subject: "Access to the #{group.name} group was granted")
+  end
 end
